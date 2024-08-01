448d0493e3d90c8c7bb2189bd28daf9b3754d9f1
evtest-hash <84323112+evtest-hash@users.noreply.github.com>
2024-08-01 11:36:53 +0800

    Fix the problem that Terminal command with sudo takes a long time

--- config/boards/mixtile-blade3.sh
+++ config/boards/mixtile-blade3.sh
@@ -63,6 +63,15 @@ sed -i -r \
 ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
 echo "Etc/UTC" >/etc/timezone
 echo "mixtile-ubuntu" >/etc/hostname
+
+echo "127.0.0.1   localhost
+127.0.1.1   mixtile-ubuntu
+::1         localhost mixtile-ubuntu ip6-localhost ip6-loopback
+fe00::0     ip6-localnet
+ff00::0     ip6-mcastprefix
+ff02::1     ip6-allnodes
+ff02::2     ip6-allrouters" > /etc/hosts
+
 EOF
 
     # Fix and configure audio device
